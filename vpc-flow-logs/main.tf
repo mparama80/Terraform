b93156ceebd0f19f5d47fb2bcf3ad53ae49b4498
mparama80 <41685194+mparama80@users.noreply.github.com>
2025-05-13 16:14:33 +0530

    Update main.tf

--- vpc-flow-logs/main.tf
+++ vpc-flow-logs/main.tf
@@ -14,47 +14,6 @@ resource "aws_flow_log" "cn-northwest-infra-vpc" {
    vpc_id               = "vpc-0e8908e9ac9b278ea"
    log_destination_type = "s3"
 }
-resource "aws_s3_bucket_policy" "vpc_flow_logs_policy" {
-    bucket = aws_s3_bucket.flow_logs.id
-
-    policy = jsonencode({
-        Version = "2012-10-17",
-        Statement = [
-            {
-                Effect = "Allow",
-                Principal = {
-                    Service = "vpc-flow-logs.amazonaws.com"
-                },
-                Action = "s3:PutObject",
-                Resource = "arn:aws:s3:::flow-logs-bucketvpc07457e1-cn-northwest-1/AWSLogs/*"
-            },
-            {
-                Effect = "Deny",
-                Principal = "*",
-                Action = [
-                    "s3:DeleteObject",
-                    "s3:DeleteObjectVersion"
-                ],
-                Resource = "arn:aws:s3:::flow-logs-bucketvpc07457e1-cn-northwest-1/AWSLogs/*"
-            },
-            {
-                Effect = "Allow",
-                Principal = {
-                    AWS = [
-                        "arn:aws:iam::024848447104:user/paramasivam"
-                    ]
-                },
-                Action = [
-                    "s3:PutObjectAcl",
-                    "s3:PutObjectVersionAcl",
-                    "s3:PutObjectTagging",
-                    "s3:PutObjectVersionTagging"
-                ],
-                Resource = "arn:aws:s3:::flow-logs-bucketvpc07457e1-cn-northwest-1/AWSLogs/*"
-            }
-        ]
-    })
-}
 terraform {
   backend "s3" {
     bucket         = "flow-logs-bucketvpc07457e1-cn-northwest-1"
